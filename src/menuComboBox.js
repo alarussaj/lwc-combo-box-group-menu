cdee03f647624a04b61e4228def940e109e4f559
alarussaj <11298805+undefined@users.noreply.github.com>
2021-12-06 14:47:56 +0000

    Add scrolling and keyboard interactions

--- src/menuComboBox.js
+++ src/menuComboBox.js
@@ -1,47 +1,71 @@
 import { LightningElement, api, track } from "lwc";
+import { scrollIntoViewIfNeeded } from "c/keyboardUtils";
 
 const KEY_ARROW_UP = 38;
 const KEY_ARROW_DOWN = 40;
 const KEY_ENTER = 13;
 const KEY_ESCAPE = 27;
+const MAIN_BUTTON = 0;
 
 export default class MenuComboBox extends LightningElement {
   // Public properties
   @api label = "";
   @api placeholder = "";
+  @api required = false;
+
+  // Template properties
+  error;
+
+  // Private properties
+  @track _menuOptions = [];
+  _isFirstRender = true;
+  _hasFocus = false;
+  _cancelBlur = false;
+  _focusedResultIndex = null;
+  _searchResultsCount = 0;
+  _listBox;
+  _value;
+  _isDirty = false;
+
+  // PUBLIC GETTERS/SETTERS
 
   get menuOptions() {
     return this._menuOptions ?? [];
   }
 
-  get value() {
-    return this._value ?? null;
-  }
-
   @api
   set menuOptions(menuOptions = []) {
     const self = this;
-    [...menuOptions].forEach((menuOption, i) => {
-      const options = menuOption.options.map((option, j) => ({
-        ...option,
-        selected: false,
-        id: i > 0 ? i + j + i : i + j,
-        get classes() {
-          let cls =
-            "menu-option-options slds-media slds-listbox__option slds-listbox__option_entity slds-listbox__option_has-meta menu-options";
-          if (self._focusedResultIndex === (i > 0 ? i + j + i : i + j)) {
-            cls += " slds-has-focus";
-          }
-          return cls;
-        },
-      }));
+    let i = 0;
+    [...menuOptions].forEach((menuOption) => {
+      const options = menuOption.options.map((item) => {
+        const id = i;
+        const option = {
+          ...item,
+          selected: false,
+          id: id,
+          get classes() {
+            let cls =
+              "menu-option-options slds-media slds-listbox__option slds-listbox__option_entity slds-listbox__option_has-meta menu-options";
+            if (self._focusedResultIndex === id) {
+              cls += " slds-has-focus";
+            }
+            return cls;
+          },
+        };
+        i = i + 1;
+        return option;
+      });
       this._searchResultsCount += options.length;
       this._menuOptions.push({
         name: menuOption.name,
         options: options,
       });
     });
-    this._defaultMenuOptions = this._menuOptions;
+  }
+
+  get value() {
+    return this._value ?? null;
   }
 
   get selectedOptions() {
@@ -55,28 +79,9 @@ export default class MenuComboBox extends LightningElement {
     return this.selectedOptions.length;
   }
 
-  get ariaExapanded() {
+  get ariaExpanded() {
     return this._hasFocus;
   }
-
-  renderedCallback() {
-    if (this._isFirstRender) {
-      this._isFirstRender = false;
-      this._listBox = this.template.querySelector('[role="listbox"]');
-    }
-  }
-
-  // Private properties
-  @track _menuOptions = [];
-  @track __defaultMenuOptions;
-  _isFirstRender = true;
-  _hasFocus = false;
-  _cancelBlur = false;
-  _focusedResultIndex = null;
-  _searchResultsCount = 0;
-  _listBox;
-  _value;
-
 
   get searchResultsCount() {
     let total = 0;
@@ -89,14 +94,22 @@ export default class MenuComboBox extends LightningElement {
     return total;
   }
 
-  
+  // CALLBACKS
+
+  renderedCallback() {
+    if (this._isFirstRender) {
+      this._isFirstRender = false;
+      this._listBox = this.template.querySelector('[role="listbox"]');
+    }
+  }
 
   // INTERNAL FUNCTIONS
 
-  hasSelection() {
-    return this._value;
-  }
-
+  /**
+   * Sets the combo box value and dispatches value
+   * @param {string} selectedValue value selected
+   * @param {string} optionLabel label of value selected
+   */
   setSelection(selectedValue, optionLabel) {
     this._value = optionLabel;
     this._menuOptions?.forEach((menuOption) => {
@@ -106,63 +119,26 @@ export default class MenuComboBox extends LightningElement {
       });
     });
     this._hasFocus = false;
+    this._isDirty = true;
     const selectionEvent = new CustomEvent("select", {
       detail: selectedValue,
     });
     this.dispatchEvent(selectionEvent);
   }
 
-  search(newSearchTerm) {
-    this._searchTerm = newSearchTerm;
-    const newCleanSearchTerm = newSearchTerm
-      .trim()
-      .replace(/\*/g, "")
-      .toLowerCase();
-    if (this._cleanSearchTerm === newCleanSearchTerm) {
-      return;
-    }
-    this._cleanSearchTerm = newCleanSearchTerm;
-    if (newCleanSearchTerm) {
-      this._searchResultsCount = 0;
-      const menuOptions = [];
-      [...this._menuOptions].forEach((menuOption) => {
-        const options =
-          menuOption.options.filter((option) => {
-            return option.label.toLowerCase().indexOf(newCleanSearchTerm) > -1;
-          }) ?? [];
-        menuOptions.push({
-          name: menuOption.name,
-          options: options,
-        });
-      });
-      this._menuOptions = menuOptions;
-    } else {
-      this._menuOptions = this._defaultMenuOptions;
-    }
-  }
-
-  scrollIntoViewIfNeeded(element, scrollingParent) {
-    const parentRect = scrollingParent.getBoundingClientRect();
-    const findMeRect = element.getBoundingClientRect();
-    if (findMeRect.top < parentRect.top) {
-      if (element.offsetTop + findMeRect.height < parentRect.height) {
-        scrollingParent.scrollTop = 0;
-      } else {
-        scrollingParent.scrollTop = element.offsetTop;
-      }
-    } else if (findMeRect.bottom > parentRect.bottom) {
-      scrollingParent.scrollTop += findMeRect.bottom - parentRect.bottom;
-    }
-  }
-
   // EVENT HANDLING
 
+  /**
+   * Input focus
+   */
   handleFocus() {
-    console.log("focus");
     this._hasFocus = true;
     this._focusedResultIndex = null;
   }
 
+  /**
+   * Input blur
+   */
   handleBlur() {
     if (this._cancelBlur) {
       return;
@@ -170,13 +146,32 @@ export default class MenuComboBox extends LightningElement {
     this._hasFocus = false;
   }
 
-  handleInput(event) {
+  /**
+   * Input value
+   */
+  handleInput() {
     this._value = null;
   }
 
+  /**
+   * Input mouse down event
+   * @param {Object} event
+   */
+  handleMouseDown(event) {
+    if (event.button === MAIN_BUTTON && !this._hasFocus) {
+      console.log("button pressed");
+      this._hasFocus = true;
+    }
+  }
+
+  /**
+   * Handles the input key down using keyboard events that allow selection and scrolling
+   * @param {object} event
+   */
   handleKeyDown(event) {
+    console.log("this._focusedResultIndex", this._focusedResultIndex);
     this._hasFocus = true;
-    if (event.keyCode == KEY_ESCAPE) {
+    if (event.keyCode === KEY_ESCAPE) {
       this._hasFocus = false;
       return;
     }
@@ -201,9 +196,9 @@ export default class MenuComboBox extends LightningElement {
       this._hasFocus &&
       this._focusedResultIndex >= 0
     ) {
-      this.template.querySelector(
-        `[data-option-id="${this._focusedResultIndex}"]`
-      )?.click();
+      this.template
+        .querySelector(`[data-option-id="${this._focusedResultIndex}"]`)
+        ?.click();
       event.preventDefault();
     }
     if (
@@ -214,33 +209,53 @@ export default class MenuComboBox extends LightningElement {
       const optionSelected = this.template.querySelector(
         `[data-option-id="${this._focusedResultIndex}"]`
       );
-      this.scrollIntoViewIfNeeded(optionSelected, this._listBox);
+      scrollIntoViewIfNeeded(optionSelected, this._listBox);
     }
   }
 
+  /**
+   * Mouse down event that sets the input value and focus
+   * @param {Object} event
+   */
   handleComboboxMouseDown(event) {
-    const mainButton = 0;
-    if (event.button === mainButton) {
+    if (event.button === MAIN_BUTTON) {
       this._cancelBlur = true;
     }
   }
 
+  /**
+   * Mouse up even that handles the input focus
+   */
   handleComboboxMouseUp() {
     this._cancelBlur = false;
-    this.template.querySelector("input").focus();
+    this._hasFocus = true;
   }
 
+  /**
+   * Selects an option and dispatches the selected value
+   * @param {Object} event
+   */
   handleOptionClick(event) {
-    const { value: selectedValue, name: optionLabel } = event.currentTarget.dataset;
+    const { value: selectedValue, name: optionLabel } =
+      event.currentTarget.dataset;
     this.setSelection(selectedValue, optionLabel);
   }
 
   // STYLE EXPRESSIONS
 
+  get inputClass() {
+    const css = ["slds-input slds-combobox__input caret-transparent"];
+    css.push([
+      this.required && this._isDirty && !this._value ? "has-custom-error" : "",
+    ]);
+    return css.join(" ");
+  }
+
   get comboxBoxClass() {
-    let css =
-      "slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click ";
-    css += this._hasFocus ? "slds-is-open" : "";
-    return css;
+    const css = [
+      "slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click",
+    ];
+    css.push([this._hasFocus ? "slds-is-open" : ""]);
+    return css.join(" ");
   }
 }
